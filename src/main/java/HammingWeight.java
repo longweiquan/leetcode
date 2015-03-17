28b01a6172612d9738b9c635a2244f6910f669bd
longweiquan <longweiquan@gmail.com>
2015-03-17 16:05:58 +0100

    HammingWeight

---
+++ src/main/java/HammingWeight.java
@@ -0,0 +1,17 @@
+/**
+ * Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+ *
+ * For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+ */
+public class HammingWeight {
+
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int result = 0;
+        while(n != 0){
+            result += (n & 1);
+            n = n >>> 1;
+        }
+        return result;
+    }
+}
