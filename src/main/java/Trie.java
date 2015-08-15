b67ef9ac6198de676b07b4dbb0a8c732b1d93109
longweiquan <longweiquan@gmail.com>
2015-08-15 10:53:29 +0200

    Add WordSearchII

--- src/main/java/Trie.java
+++ src/main/java/Trie.java
@@ -1,3 +1,8 @@
+import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by longweiquan on 15/7/25.
  */
@@ -9,31 +14,65 @@ public class Trie {
         root = new TrieNode();
     }
 
-    // Inserts a word into the trie.
     public void insert(String word) {
-        root.insert(word);
-    }
 
-    // Returns if the word is in the trie.
-    public boolean search(String word) {
-        return true;
-    }
+        TrieNode current = root;
 
-    // Returns if there is any word in the trie
-    // that starts with the given prefix.
-    public boolean startsWith(String prefix) {
-        return true;
-    }
+        for(int i=0;i<word.length();i++){
 
-    public static class TrieNode {
+            char c = word.charAt(i);
+            if(!current.children.containsKey(c)){
+                TrieNode newNode = new TrieNode();
+                newNode.value = c;
+                current.children.put(c, newNode);
+            }
 
-        private TrieNode[] children;
-        private char value;
+            current = current.children.get(c);
 
-        public void insert(String word){
-
+            if(i+1 == word.length()){
+                current.isLeaf = true;
+            }
         }
-
-
     }
+
+    public boolean search(String word) {
+
+        TrieNode current = getTrieNodeByPrefix(word);
+        return current != null && current.isLeaf;
+    }
+
+    public boolean startsWith(String prefix) {
+        return getTrieNodeByPrefix(prefix)  != null;
+    }
+
+    public TrieNode getTrieNodeByPrefix(String prefix) {
+        TrieNode current = root;
+
+        for(int i=0;i<prefix.length();i++){
+
+            char c = prefix.charAt(i);
+            current = current.children.get(c);
+            if(current == null){
+                break;
+            }
+        }
+        return current;
+    }
+
+    public class TrieNode {
+
+        char value;
+        Map<Character, TrieNode> children = new HashMap<>();
+        boolean isLeaf = false;
+    }
+
+    public static void main(String[] args) {
+
+        Trie trie = new Trie();
+        trie.insert("something");
+        Assert.assertTrue(trie.startsWith("some"));
+        Assert.assertFalse(trie.search("some"));
+        Assert.assertTrue(trie.search("something"));
+    }
+
 }
---
+++ src/main/java/WordSearchII.java
@@ -0,0 +1,89 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+
+import javax.swing.text.Position;
+import java.util.*;
+
+/**
+ * Created by longweiquan on 15/7/19.
+ *
+ * Solution : Depth First Search with Trie to optimize the word comparison
+ *
+ */
+public class WordSearchII {
+
+    private Set<String> results;
+    private char[][] board;
+    private Trie trie;
+    boolean[][] visited;
+
+    public List<String> findWords(char[][] board, String[] words) {
+
+        // false input
+        if(board == null || board.length == 0 || words == null || words.length == 0){
+            return new ArrayList<String>();
+        }
+
+        this.board = board;
+        int X = board.length;
+        int Y = board[0].length;
+        visited = new boolean[X][Y];
+        results = new HashSet<>();
+
+
+        // build Trie
+        trie = new Trie();
+        for(String word: words){
+            trie.insert(word);
+        }
+
+
+        // for each character in the table
+        for(int i=0; i<X; i++){
+            for(int j=0;j<Y;j++){
+                dfs("", i, j);
+            }
+        }
+
+        return new ArrayList<>(results);
+    }
+
+    private void dfs(String word, int x, int y){
+
+        if(x<0 || x>=board.length || y<0 || y>=board[0].length || visited[x][y]){
+            return;
+        }
+
+        word += board[x][y];
+        if(trie.startsWith(word)){
+
+            if(trie.search(word)){
+                results.add(word);
+            }
+
+            visited[x][y] = true;
+            dfs(word, x-1, y);
+            dfs(word, x+1, y);
+            dfs(word, x, y-1);
+            dfs(word, x, y+1);
+            visited[x][y] = false;
+        }
+    }
+
+    public static void main(String[] args){
+        WordSearchII solution = new WordSearchII();
+
+        char[][] board = {
+            {'o','a','a','n'},
+            {'e','t','a','e'},
+            {'i','h','k','r'},
+            {'i','f','l','v'}
+        };
+        String[] words = {"oath","pea","eat","rain"};
+        Assertions.assertThat(solution.findWords(board, words)).containsOnlyElementsOf(Arrays.asList("eat", "oath"));
+
+        board = new char[][]{{'b','a','b','b','a'}};
+        words = new String[]{"baa","abba","baab","aba"};
+        Assertions.assertThat(solution.findWords(board, words)).containsOnlyElementsOf(Arrays.asList("abba"));
+    }
+}
